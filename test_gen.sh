f16aceb14d0d13a63c5283830b37eef4d9bcb95e
rqzhangberkeley <meisong541@gmail.com>
2024-09-22 16:22:25 -0700

    update-0922-GPUnumber

--- test_gen.sh
+++ test_gen.sh
@@ -2,7 +2,7 @@
 # First approach: initialize 4 VLLM processes and split the prompt set to the 4 agents
 # The generated samples will be stored at output_dir + local_index + ".json
 
-my_world_size=8 # how many GPUs you use
+my_world_size=2 # how many GPUs you use
 infer_model='RLHFlow/LLaMA3-SFT' # In the first iteration, we use Llama3-SFT model.
 prompt_dir='RLHFlow/iterative-prompt-v1-iter1-20K'
 output_folder='../Outputs-Online-RLHF/iterative-prompt-v1-iter1-20K'
@@ -17,10 +17,10 @@ else
 fi
 
 # Array of GPU IDs to be used......
-declare -a gpus=(0 1 2 3 4 5 6 7)
+declare -a gpus=(0 1)
 
 # conda activate vllm
-for i in {0..7}; do
+for i in {0..1}; do
     CUDA_VISIBLE_DEVICES=${gpus[$i]} python ./generation/get_hf2.py --model_name_or_path ${infer_model} \
                                                                     --dataset_name_or_path ${prompt_dir} \
                                                                     --output_dir ${output_dir} \
